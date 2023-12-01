19d8eff3841b60eba9b0b8995f1ccf1aaf0e0ca7
ChristianJoyGaray <cj.garayhello@yahoo.com>
2023-12-01 13:07:25 +0800

    new

--- script.js
+++ script.js
@@ -1561,3 +1561,16 @@ console.log(odin.targetPlanet)
 
 
 //another example////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function makeClass(){
+    class vegetable{
+        constructor (name){
+            this.name = name;
+        }
+    }
+    return vegetable;
+}
+
+const vegetable = makeClass();
+const carrot = new vegetable('carrot');
+console.log(carrot.name);
